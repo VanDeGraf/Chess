8aa0d5250a861d2db19600310079f34b649b60ae
VanDeGraf <vandegraf@bk.ru>
2021-05-18 18:35:31 +0400

    In Coordinate class add method relative_coordinate with inputx and y, that returns new Coordinate instance with summared self + input.

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -7,6 +7,10 @@ class Coordinate
   end
 
   def to_s
-    (97 + @x).chr + (@y+1).to_s
+    (97 + @x).chr + (@y + 1).to_s
+  end
+
+  def relative_coordinate(x_add, y_add)
+    Coordinate.new(@x + x_add, @y + y_add)
   end
 end
