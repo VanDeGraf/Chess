3428e346353b65490ac613846ea6781642268c5b
VanDeGraf <vandegraf@bk.ru>
2021-05-19 21:53:17 +0400

    Add PossibleMoves class. It generate and store all possible figure moves depended by figure type, color, and coordinate. Method build_moves generate moves, and use private repeatable methods add_move, add_moves and add_move_as_part_of_row. Method match? works like filter with options, that return ask of question about contains moves by requested options.

---
+++ lib/possible_moves.rb
@@ -0,0 +1,116 @@
+class PossibleMoves
+  def initialize(figure, start, board)
+    @figure = figure
+    @start_coordinate = start
+    @moves_coordinates = []
+    @board = board
+  end
+
+  def build_moves
+    if @figure.figure == :pawn
+      move_direction = @figure.color == :white ? 1 : -1
+
+      add_move(0, 1 * move_direction) { |point|
+        @board.on_board?(point) &&
+        @board.at(point).nil?
+      }
+      add_move(0, 2 * move_direction) { |point|
+        @start_coordinate.y == 1 &&
+        @board.at(point).nil?
+      }
+      add_move(-1, 1 * move_direction) { |point|
+        !@board.at(point).nil? &&
+        @board.at(point).color != @figure.color
+      }
+      add_move(1, 1 * move_direction) { |point|
+        !@board.at(point).nil? &&
+        @board.at(point).color != @figure.color
+      }
+    elsif @figure.figure == :knight
+      add_moves([
+        [-1, 2],
+        [1, 2],
+        [-1, -2],
+        [1, -2],
+        [-2, 1],
+        [-2, -1],
+        [2, 1],
+        [2, -1],
+      ])
+    elsif @figure.figure == :king
+      add_moves([
+        [-1, 1],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [1, -1],
+        [0, -1],
+        [-1, -1],
+        [-1, 0],
+      ])
+    elsif [:queen, :rook, :bishop].include?(@figure.figure)
+      if [:queen, :rook].include?(@figure.figure)
+        add_move_as_part_of_row { |point| point.relative(1, 0) }
+        add_move_as_part_of_row { |point| point.relative(-1, 0) }
+        add_move_as_part_of_row { |point| point.relative(0, 1) }
+        add_move_as_part_of_row { |point| point.relative(0, -1) }
+      end
+      if [:queen, :bishop].include?(@figure.figure)
+        add_move_as_part_of_row { |point| point.relative(-1, 1) }
+        add_move_as_part_of_row { |point| point.relative(1, 1) }
+        add_move_as_part_of_row { |point| point.relative(1, -1) }
+        add_move_as_part_of_row { |point| point.relative(-1, -1) }
+      end
+    end
+  end
+
+  # @param color [Symbol] figure color, nil - doesn't matter
+  # @param beat [Boolean] is figure beat other in any moves? nil - doesn't matter
+  # @param beats [Symbol] figure beats this figure type, nil - doesn't matter
+  def match?(color = nil, beat = nil, beats = nil)
+    return false if !color.nil? && color != @figure.color
+    return true if beat.nil?
+    if beat == @moves_coordinates.reduce { |beat, point| beat || !@board.at(point).nil? }
+      return true if beats.nil?
+      @moves_coordinates.each do |point|
+        return true if !@board.at(point).nil? && @board.at(point).figure == beats
+      end
+      return false
+    else
+      return false
+    end
+  end
+
+  private
+
+  def add_move(x, y)
+    point = @start_coordinate.relative(x, y)
+    if block_given?
+      if yield(point)
+        @moves_coordinates << point
+        true
+      end
+    else
+      if @board.can_move_at?(@figure, point)
+        @moves_coordinates << point
+        true
+      end
+    end
+    false
+  end
+
+  def add_moves(endpoints)
+    endpoints.each do |coordinate|
+      add_move(coordinate.x, coordinate.y)
+    end
+  end
+
+  def add_move_as_part_of_row
+    point = yield @start_coordinate
+    add_move(point)
+    while @board.on_board?(point) && @board.at(point).nil?
+      point = yield point
+      add_move(point)
+    end
+  end
+end
