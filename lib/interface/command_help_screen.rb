740ba0467dd97bce1d084fab1d4fbc3b924898a0
VanDeGraf <vandegraf@bk.ru>
2021-06-21 14:15:55 +0400

    Correct the mistake with backslash in CommandHelpScreen message

--- lib/interface/command_help_screen.rb
+++ lib/interface/command_help_screen.rb
@@ -1,15 +1,15 @@
 class CommandHelpScreen < MessageScreen
   def initialize
     super(
-      "\nYou can enter commands only when it is explicitly indicated.\n\n" /
-        "\tCommand list:\n" /
-        "/save - save current game state to specified file\n" /
-        "/export - save current game state to specified file, use specified format\n" /
-        "/mm - go to main menu\n" /
-        "/draw - offer the opponent a draw\n" /
-        "/surrender - admit defeat, the opponent must accept it\n" /
-        "/history - show game turns history\n" /
-        "/help - show this screen\n" /
+      "You can enter commands only when it is explicitly indicated.\n\n" \
+        "\tCommand list:\n" \
+        "/save - save current game state to specified file\n" \
+        "/export - save current game state to specified file, use specified format\n" \
+        "/mm - go to main menu\n" \
+        "/draw - offer the opponent a draw\n" \
+        "/surrender - admit defeat, the opponent must accept it\n" \
+        "/history - show game turns history\n" \
+        "/help - show this screen\n" \
         "/quit - exit from this application\n"
     )
     @header = 'Commands'
