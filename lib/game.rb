d3f62e2f68a3a2f44f92bf4067e4c7026fd45fc4
VanDeGraf <vandegraf@bk.ru>
2021-06-26 17:57:40 +0400

    Add import\export PGN save files methods to Game class
    Add EXPORT_DIR const with same logic as SAVE_DIR.
    Add #full_name_of_export_file method with same logic as #full_name_of_save_file.
    Add #export method, that write in file passed game status to 7 default PGN tags and every movement from board history in algebraic notation with turn numeration and marks shah and mate status.
    Add #parse_pgn_tags method that return from passed PGN save string all tags with values like Hash.
    Add #split_pgn_to_saves method that return array of PGN save strings with short description of each, contained in passed file.
    Add #import method takes PGN save as string that pass game state from tags to new instance, convert and do every movement on board to simulate real game and return resulted game instance.

--- lib/game.rb
+++ lib/game.rb
@@ -88,6 +88,7 @@ class Game
   end
 
   SAVE_DIR = File.expand_path('../saves', __dir__).freeze
+  EXPORT_DIR = File.expand_path('../saves', __dir__).freeze
 
   def save(filename)
     Dir.mkdir(SAVE_DIR) unless Dir.exist?(SAVE_DIR)
@@ -103,6 +104,14 @@ class Game
     "#{SAVE_DIR}/#{filename}.yaml"
   end
 
+  # @param filename [String]
+  # @return [String]
+  def self.full_name_of_export_file(filename)
+    return filename if filename.include?(EXPORT_DIR)
+
+    "#{EXPORT_DIR}/#{filename}.pgn"
+  end
+
   # @return [Game, nil]
   def self.load(filename)
     filename = full_name_of_save_file(filename)
@@ -112,5 +121,129 @@ class Game
                      Game, Board, Figure, Player, Movement, Move, Capture, EnPassant, PromotionCapture, PromotionMove,
                      Castling, Coordinate, Symbol, Computer
                    ], aliases: true)
+  end
+
+  def export(filename)
+    Dir.mkdir(EXPORT_DIR) unless Dir.exist?(EXPORT_DIR)
+    filename = Game.full_name_of_export_file(filename)
+    buffer = StringIO.new
+    buffer << "[Event \"Game\"]\n"
+    buffer << "[Site \"?\"]\n"
+    buffer << "[Date \"????.??.??\"]\n"
+    buffer << "[Round \"?\"]\n"
+    buffer << "[White \"#{@players[0].name}\"]\n"
+    buffer << "[Black \"#{@players[1].name}\"]\n"
+    game_result = if @finished
+                    if @winner.nil?
+                      '1/2-1/2'
+                    else
+                      @winner == @players[0] ? '1-0' : '0-1'
+                    end
+                  else
+                    '*'
+                  end
+    buffer << "[Result \"#{game_result}\"]\n\n"
+
+    row = StringIO.new
+    current_board_state = Board.new
+    current_color = :white
+    @board.history.each_with_index do |movement, i|
+      if i.even?
+        if row.length >= 80
+          buffer << "#{row.string}\n"
+          row = StringIO.new
+        end
+        row << "#{(i + 2) / 2}."
+      end
+
+      possible_moves = (current_board_state.where_is(nil, current_color).map do |coordinate|
+        MovementGenerator.generate_from(coordinate, current_board_state)
+      end + MovementGenerator.castling(current_board_state, current_color)).flatten
+      player_turn = nil
+      assoc = current_board_state.algebraic_notation(possible_moves)
+      assoc.each_pair do |string, move|
+        next if move != movement
+
+        player_turn = string
+        break
+      end
+
+      current_board_state = current_board_state.move(movement)
+      current_color = current_color == :white ? :black : :white
+
+      postfix = current_board_state.mate?(current_color) ? '#' : ''
+      postfix = current_board_state.shah?(current_color) ? '+' : '' if postfix.length.zero?
+
+      row << "#{player_turn}#{postfix} "
+    end
+    row << " #{game_result}" if @finished
+    buffer << row.string
+
+    File.write(filename, buffer.string)
+  end
+
+  # @param filename [String]
+  # @return [Hash<Array<String>>]
+  def self.split_pgn_to_saves(filename)
+    data = File.read(filename)
+    blocks = data.delete("\r").split("\n\n")
+    data = {
+      saves: []
+    }
+    blocks.each_index do |i|
+      data[:saves] << blocks[i - 1] + blocks[i] if i.odd?
+    end
+    data[:descriptions] = data[:saves].map do |save_string|
+      tags = parse_pgn_tags(save_string)
+      "[#{tags['Event']}] #{tags['White']}  vs  #{tags['Black']}"
+    end
+    data
+  end
+
+  # @param pgn [String] - PGN save as string
+  # @return [Game, nil]
+  def self.import(pgn)
+    tags = parse_pgn_tags(pgn)
+    game = Game.new([
+                      Player.new(tags['White'], :white),
+                      Player.new(tags['Black'], :black)
+                    ])
+    if tags['Result'] == '*'
+      game.instance_variable_set(:@finished, false)
+      game.instance_variable_set(:@winner, nil)
+    else
+      game.instance_variable_set(:@finished, true)
+      winner = case tags['Result']
+               when '1-0'
+                 game.instance_variable_get(:@players)[0]
+               when '0-1'
+                 game.instance_variable_get(:@players)[1]
+               end
+      game.instance_variable_set(:@winner, winner)
+    end
+
+    re = /([BNQKR]?[a-h]?[1-8]?x?[a-h][1-8]=?[BNQKR]?)|(O-O)|(O-O-O)/m
+    pgn.scan(re) do |m|
+      string_move = "#{m[0]}#{m[1]}#{m[2]}"
+
+      possible_moves = (game.board.where_is(nil, game.current_player.color).map do |coordinate|
+        MovementGenerator.generate_from(coordinate, game.board)
+      end + MovementGenerator.castling(game.board, game.current_player.color)).flatten
+
+      move = game.board.algebraic_notation(possible_moves)[string_move]
+      game.board.move!(move)
+
+      game.instance_variable_set(:@current_player, game.instance_variable_get(:@current_player).zero? ? 1 : 0)
+    end
+    game
+  end
+
+  def self.parse_pgn_tags(pgn)
+    tags = {}
+    re = /\[(\w+) "([^"]*)"\]/m
+    pgn.scan(re) do |match|
+      tags[match[0]] = match[1]
+    end
+    tags
   end
 end
