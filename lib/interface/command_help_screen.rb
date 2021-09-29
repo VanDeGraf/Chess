61eddb5f864ff12fac26c7ebd3dfb11484977c64
VanDeGraf <vandegraf@bk.ru>
2021-09-29 10:34:35 +0400

    Move text of help message screen to file

--- lib/interface/command_help_screen.rb
+++ lib/interface/command_help_screen.rb
@@ -1,21 +1,9 @@
 class CommandHelpScreen < MessageScreen
+  HELP_FILE = File.expand_path('help.txt', __dir__).freeze
+  TEXT = File.read(HELP_FILE)
+
   def initialize
-    super(
-      "You can enter commands only when it is explicitly indicated.\n\n" \
-        "\tCommand list:\n" \
-        "/save - save current game state to specified file\n" \
-        "/export - save current game state to specified file, use PGN format\n" \
-        "/mm - go to main menu\n" \
-        "/draw - offer the opponent a draw\n" \
-        "/surrender - admit defeat, the opponent must accept it\n" \
-        "/history - show game turns history\n" \
-        "/help - show this screen\n" \
-        "/quit - exit from this application\n\n" \
-        "\tIn-Game turn input variants:\n" \
-        "1)Simple, example: `a2 a4`,`b1 c3`\n" \
-        "2)Algebraic Notation, example: `a4`,`Nc3`\n" \
-        "3)Choose special move, if it exists, just with type number of move\n"
-    )
+    super(TEXT)
     @header = 'Commands'
   end
 
