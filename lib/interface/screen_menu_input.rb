99326674731da3bba4cc5c69d1a8412a39f1efcc
VanDeGraf <vandegraf@bk.ru>
2021-09-30 08:31:42 +0400

    Refactoring divide method handle_console_input

--- lib/interface/screen_menu_input.rb
+++ lib/interface/screen_menu_input.rb
@@ -17,22 +17,31 @@ class ScreenMenuInput < ScreenInput
   # @param screen_draw_method [Proc]
   # @return [Symbol, Integer, String]
   def handle_console_input(screen_draw_method)
-    handled = nil
-    re = /^\d+$/
+    input = nil
     loop do
       input = gets.chomp
-      unless re.match?(input)
-        @error_message = "String must follow this regexp filter: #{re}"
-        screen_draw_method.call
-        next
-      end
-      unless (handled = input.to_i).between?(1, @actions.length)
-        @error_message = "Number must be between 1 and #{@actions.length}"
-        screen_draw_method.call
-        next
-      end
+      next unless regexp_verify?(input, screen_draw_method) &&
+                  number_range_verify?(input, screen_draw_method)
+
       break
     end
-    @actions[handled.to_i - 1].name
+    @actions[input.to_i - 1].name
+  end
+
+  def regexp_verify?(input, screen_draw_method)
+    re = /^\d+$/
+    return true if re.match?(input)
+
+    @error_message = "String must follow this regexp filter: #{re}"
+    screen_draw_method.call
+    false
+  end
+
+  def number_range_verify?(input, screen_draw_method)
+    return true if input.to_i.between?(1, @actions.length)
+
+    @error_message = "Number must be between 1 and #{@actions.length}"
+    screen_draw_method.call
+    false
   end
 end
