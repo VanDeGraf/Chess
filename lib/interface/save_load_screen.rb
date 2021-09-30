07f502269c6d847c3622d6c9f612f7e54c48292b
VanDeGraf <vandegraf@bk.ru>
2021-09-30 08:46:08 +0400

    Refactoring divide method handle_input, move filters to new method

--- lib/interface/save_load_screen.rb
+++ lib/interface/save_load_screen.rb
@@ -2,20 +2,22 @@ class SaveLoadScreen < Screen
   def initialize(type, game: nil)
     super(HEADERS[type],
           input: ScreenDataInput.new(INPUT_DESCRIPTIONS[type],
-                                     filters: [
-                                       InputFilter.new(/^[a-zA-Z \d_]+$/, handler: proc { |match_data|
-                                         filename = match_data.to_s
-                                         if @type == :load && !File.exist?(SaveSerializer.new.filepath(filename))
-                                           InputFilter.error_result('Save file with this name not found!')
-                                         elsif @type == :import && !File.exist?(PGNSerializer.new.filepath(filename))
-                                           InputFilter.error_result('PGN formatted save file with this name not found!')
-                                         else
-                                           filename
-                                         end
-                                       })
-                                     ]))
+                                     filters: init_data_input_filters))
     @type = type
     @game = game
+  end
+
+  def init_data_input_filters
+    [InputFilter.new(/^[a-zA-Z \d_]+$/, handler: proc { |match_data|
+      filename = match_data.to_s
+      if @type == :load && !File.exist?(SaveSerializer.new.filepath(filename))
+        InputFilter.error_result('Save file with this name not found!')
+      elsif @type == :import && !File.exist?(PGNSerializer.new.filepath(filename))
+        InputFilter.error_result('PGN formatted save file with this name not found!')
+      else
+        filename
+      end
+    })]
   end
 
   # @param type [Symbol] :save or :load
@@ -28,48 +30,65 @@ class SaveLoadScreen < Screen
   end
 
   def handle_input
-    loop do
+    command_result = nil
+    while command_result.nil?
       filename = @input.handle_console_input(-> { draw })
-      case @type
-      when :save
-        ss = SaveSerializer.new
-        if File.exist?(ss.filepath(filename)) && !AcceptRequestScreen.show_and_read(
-          'File with this name already exists, do you want rewrite it?'
-        )
-          draw
-          next
-        end
-        ss.serialize(@game, filename)
-        MessageScreen.show("Game saved successfully to #{ss.filepath(filename)}")
-        return @game
-      when :load
-        return SaveSerializer.new.deserialize(filename)
-      when :export
-        pgns = PGNSerializer.new
-        if File.exist?(pgns.filepath(filename)) && !AcceptRequestScreen.show_and_read(
-          'File with this name already exists, do you want rewrite it?' \
-            '(Export save one game to one file only!)'
-        )
-          draw
-          next
-        end
-        pgns.serialize(@game, filename)
-        MessageScreen.show("Game saved successfully as PGN formatted to #{pgns.filepath(filename)}")
-        return @game
-      when :import
-        games = PGNSerializer.new.deserialize(filename)
-        return games unless games.is_a?(Array)
-
-        descriptions = []
-        games.each { |game| descriptions << game.description }
-        choice = MultipleChoiceRequestScreen.show_and_read(
-          descriptions,
-          "In this PGN file founded more then one game save!\n" \
-            'Here each: {White Player Name} vs {Black Player Name} : {Status}'
-        )
-        return games[choice]
-      end
+      command_result = perform_command(filename)
     end
+    command_result
+  end
+
+  def perform_command(filename)
+    case @type
+    when :save
+      handle_save_command(filename)
+    when :load
+      SaveSerializer.new.deserialize(filename)
+    when :export
+      handle_export_command(filename)
+    when :import
+      handle_export_command(filename)
+    end
+  end
+
+  def handle_save_command(filename)
+    ss = SaveSerializer.new
+    if File.exist?(ss.filepath(filename)) && !AcceptRequestScreen.show_and_read(
+      'File with this name already exists, do you want rewrite it?'
+    )
+      draw
+      return nil
+    end
+    ss.serialize(@game, filename)
+    MessageScreen.show("Game saved successfully to #{ss.filepath(filename)}")
+    @game
+  end
+
+  def handle_export_command(filename)
+    pgns = PGNSerializer.new
+    if File.exist?(pgns.filepath(filename)) && !AcceptRequestScreen.show_and_read(
+      'File with this name already exists, do you want rewrite it?'
+    )
+      draw
+      return nil
+    end
+    pgns.serialize(@game, filename)
+    MessageScreen.show("Game saved successfully as PGN formatted to #{pgns.filepath(filename)}")
+    @game
+  end
+
+  def handle_import_command(filename)
+    games = PGNSerializer.new.deserialize(filename)
+    return games unless games.is_a?(Array)
+
+    descriptions = []
+    games.each { |game| descriptions << game.description }
+    choice = MultipleChoiceRequestScreen.show_and_read(
+      descriptions,
+      "In this PGN file founded more then one game save!\n" \
+            'Here each: {White Player Name} vs {Black Player Name} : {Status}'
+    )
+    games[choice]
   end
 
   HEADERS = {
