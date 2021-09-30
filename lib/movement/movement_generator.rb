3bb5b459b1d456526b3a3923e2df66f1618da423
VanDeGraf <vandegraf@bk.ru>
2021-09-30 08:01:14 +0400

    Correct the mistake with call point_end on Castling instance

--- lib/movement/movement_generator.rb
+++ lib/movement/movement_generator.rb
@@ -56,8 +56,9 @@ module MovementGenerator
   end
 
   def self.rank_file_required?(move, movement)
-    false if movement == move || move.point_end != movement.point_end
-    false if !move.is_a?(Move) && !move.is_a?(Capture) && !move.is_a?(PromotionCapture)
+    return false if !move.is_a?(Move) && !move.is_a?(Capture) && !move.is_a?(PromotionCapture)
+    return false if movement == move || move.point_end != movement.point_end
+
     movement.figure.figure == move.figure.figure
   end
 
